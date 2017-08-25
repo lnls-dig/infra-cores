f36222b0cf29da14f0efdc1e4b799cdc03c854c4
Lucas Russo <lerwys@gmail.com>
2017-08-25 16:14:28 -0300

    modules/*/wb_afc_mgmt: rename define header file to wb_*
    This is our convention in software, so we fix it here.

--- modules/wishbone/wb_afc_mgmt/wbgen/build_wb.sh
+++ modules/wishbone/wb_afc_mgmt/wbgen/build_wb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wbgen2 -V afc_mgmt_regs.vhd -H record -p afc_mgmt_regs_pkg.vhd -K ../../../../sim/regs/afc_mgmt_regs.vh -s defines -C afc_mgmt_regs.h -f html -D doc/afc_mgmt_regs_wb.html afc_mgmt_regs.wb
+wbgen2 -V afc_mgmt_regs.vhd -H record -p afc_mgmt_regs_pkg.vhd -K ../../../../sim/regs/afc_mgmt_regs.vh -s defines -C wb_afc_mgmt_regs.h -f html -D doc/afc_mgmt_regs_wb.html afc_mgmt_regs.wb
