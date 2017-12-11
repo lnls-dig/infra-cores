cb2bb624b1219cc72be97819d014eaff6fe80bfe
Lucas Russo <lerwys@gmail.com>
2017-12-11 15:47:20 -0200

    platform/*/common/Manifest.py: fix wrong "modules" syntax

--- platform/xilinx/common/Manifest.py
+++ platform/xilinx/common/Manifest.py
@@ -1,3 +1,7 @@
 files = ["platform_generic_pkg.vhd"]
 
-modules = {"local" : "iobuf_generic", "iobufds_generic"}
+modules = {"local" : [
+                        "iobuf_generic",
+                        "iobufds_generic"
+                     ]
+          }
