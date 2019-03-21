2d70de22b96bcc513c107042b6b78a2c43099f4a
Lucas Russo <lerwys@gmail.com>
2019-03-21 10:48:59 -0300

    platform/*/ddr_core: change workflow to vhd/verilog files instead of XCI
    XCI files were giving too much trouble in our workflow.
    Every Vivado version forced to change IPs and to
    recompile everything. Plus, the "preferred language"
    issue (https://www.xilinx.com/support/answers/51041.html)

--- platform/xilinx/artix7/afc_v3/ddr_core/Manifest.py
+++ platform/xilinx/artix7/afc_v3/ddr_core/Manifest.py
@@ -1,9 +1,6 @@
-if (action == "synthesis"):
-    files = ["ddr_core.xci"]
-
-else:
-    files = ["ddr_core/user_design/rtl/ddr_core.v",
+files = ["ddr_core/user_design/rtl/ddr_core.v",
          "ddr_core/user_design/rtl/ddr_core_mig_sim.v",
+         "ddr_core/user_design/rtl/ddr_core_mig.v",
          "ddr_core/user_design/rtl/axi/",
          "ddr_core/user_design/rtl/clocking/",
          "ddr_core/user_design/rtl/controller/",
