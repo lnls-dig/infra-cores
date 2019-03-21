3b2ceb0875a58a0c50ee31bc5b01b93cbc477374
Lucas Russo <lerwys@gmail.com>
2019-03-21 10:42:32 -0300

    platform/*/axi_datamover_0: change workflow to vhd/verilog files instead of XCI
    XCI files were giving too much trouble in our workflow.
    Every Vivado version forced to change IPs and to recompile
    everything. Plus, the "preferred language" issue
    (https://www.xilinx.com/support/answers/51041.html)

--- platform/xilinx/artix7/afc_v3/axi_datamover_0/Manifest.py
+++ platform/xilinx/artix7/afc_v3/axi_datamover_0/Manifest.py
@@ -1,12 +1,2 @@
-if (action == "synthesis"):
-    files = ["axi_datamover_0.xci"]
-
-else:
-    files = ["synth/",
-#            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
-#            "fifo_generator_v12_0/simulation/",
-#            "lib_cdc_v1_0/hdl/src/vhdl/",
-#            "lib_fifo_v1_0/hdl/src/vhdl/",
-#            "lib_pkg_v1_0/hdl/src/vhdl/",
-#            "lib_srl_fifo_v1_0/hdl/src/vhdl/",
-            "sim/"]
+files = ["synth/",
+         "hdl/"]
