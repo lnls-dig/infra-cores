69ab55b0daebb73c17d6320bde57a2baa7c69eee
Lucas Russo <lerwys@gmail.com>
2019-03-21 12:43:25 -0300

    Revert "platform/*/axi_interconnect_bpm: change workflow to vhd/verilog files instead of XCI"
    We can't seem to compile this module easily with the
    generated files. There are encrypted files and
    others issues.
    
    This reverts commit 24eabb29f2726102a1b99ec8e66437771706587e.

--- platform/xilinx/artix7/afc_v3/axi_interconnect_bpm/Manifest.py
+++ platform/xilinx/artix7/afc_v3/axi_interconnect_bpm/Manifest.py
@@ -1,2 +1,7 @@
-files = ["synth/",
-         "hdl/"]
+if (action == "synthesis"):
+    files = ["axi_interconnect_bpm.xci"]
+
+else:
+    files = ["synth/",
+#            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
+            "sim/"]
