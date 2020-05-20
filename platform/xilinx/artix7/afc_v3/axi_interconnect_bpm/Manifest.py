0415a38758578a5f5fcc45189d25763d645eebbe
Lucas Russo <lerwys@gmail.com>
2020-05-20 09:47:53 -0300

    platform/*/artix7: remove simulation files from Manifest as it's broken
    Those files only exist after Vivado generation. Even
    after that, hdlmake does not really support libraries, so
    we must manually compile them and include in the simulation.
    
    So, remove this so at least design not simulating this
    can correctly parse the Manifest files.

--- platform/xilinx/artix7/afc_v3/axi_interconnect_bpm/Manifest.py
+++ platform/xilinx/artix7/afc_v3/axi_interconnect_bpm/Manifest.py
@@ -1,7 +1,2 @@
 if (action == "synthesis"):
     files = ["axi_interconnect_bpm.xci"]
-
-else:
-    files = ["synth/",
-#            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
-            "sim/"]
