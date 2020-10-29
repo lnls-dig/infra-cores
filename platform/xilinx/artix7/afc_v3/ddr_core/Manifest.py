b323b4ceb069f181a8b9a7d720cb2b32bada00dd
Lucas Russo <lerwys@gmail.com>
2020-10-29 13:38:25 -0300

    platform/*/ddr_core: fix duplicate DDR MIG entity for synth/sim

--- platform/xilinx/artix7/afc_v3/ddr_core/Manifest.py
+++ platform/xilinx/artix7/afc_v3/ddr_core/Manifest.py
@@ -1,6 +1,4 @@
 files = ["ddr_core/user_design/rtl/ddr_core.v",
-         "ddr_core/user_design/rtl/ddr_core_mig_sim.v",
-         "ddr_core/user_design/rtl/ddr_core_mig.v",
          "ddr_core/user_design/rtl/axi/",
          "ddr_core/user_design/rtl/clocking/",
          "ddr_core/user_design/rtl/controller/",
@@ -8,3 +6,11 @@ files = ["ddr_core/user_design/rtl/ddr_core.v",
          "ddr_core/user_design/rtl/ip_top/",
          "ddr_core/user_design/rtl/phy/",
          "ddr_core/user_design/rtl/ui/"]
+
+if (action == "synthesis"):
+    files.extend(["ddr_core/user_design/rtl/ddr_core_mig.v"])
+elif (action == "simulation"):
+    files.extend(["ddr_core/user_design/rtl/ddr_core_mig_sim.v"])
+else:
+    import sys
+    sys.exit("ERROR: DDR_CORE: Action not recognized: {}".format(action))
