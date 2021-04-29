f17d376c966dcc2b479bc97236d590f5fcd65122
Lucas Russo <lerwys@gmail.com>
2021-04-29 14:54:18 -0300

    platform/*/artix7/Manifest.py: add board selection to use afc_v3/afc_v4

--- platform/xilinx/artix7/Manifest.py
+++ platform/xilinx/artix7/Manifest.py
@@ -2,7 +2,23 @@ modules = {
     "local": [
         "chipscope",
         "ip_cores",
-        "afc_v3",
         "buffers",
     ]
 }
+
+dir_boards_dict = {
+    "afc":      "afc_v3",
+    "afcv3":    "afc_v3",
+    "afcv4":    "afc_v4",
+}
+
+try:
+    board
+except NameError:
+    board = "afc"
+    print("board property not defined. Using default 'afc'")
+
+assert isinstance(board, str), "'board' property must be a string"
+d = dir_boards_dict.get(board, None)
+assert d is not None, "unknown name {} in 'dir_boards_dict'".format(board)
+modules["local"].append(d)
